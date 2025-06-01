56165d92d227f47f8d20dc93191a19ba5804ad0b
abdullahalhossain-bd <shahanaj7719@gmail.com>
2025-06-01 13:06:21 +0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import Index from "./pages/Index";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-background">
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
+           <Route path="/" element={<Index />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
         <Toaster />
